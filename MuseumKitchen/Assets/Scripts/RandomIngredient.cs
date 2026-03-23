04c0183e4b6c11415ae5305a986e3e7f42de1b7c
SteeleType <scitrone97@gmail.com>
2026-03-23 11:34:28 -0400

    Added More Museum Kitchen Button

--- MuseumKitchen/Assets/Scripts/RandomIngredient.cs
+++ MuseumKitchen/Assets/Scripts/RandomIngredient.cs
@@ -4,33 +4,40 @@ using UnityEngine;
 
 public class RandomIngredient : MonoBehaviour
 {
-    [SerializeField] TextMeshPro ingredientName;
+    [SerializeField] TextMeshPro fillingName;
+    [SerializeField] private TextMeshPro wrappingName;
+    [SerializeField] private TextMeshPro cookingMethodName;
+    [SerializeField] private TextMeshPro dishMade;
 
-    [SerializeField] private GameObject ingredientPrefab;
+    [SerializeField] private GameObject fillingPrefab;
+    [SerializeField] private GameObject wrappingPrefab;
+    [SerializeField] private GameObject cookingMethodPrefab;
+    [SerializeField] private GameObject dishMadePrefab;
+    
     [SerializeField] List<Ingredient> fillingList = new List<Ingredient>();
     [SerializeField] List<Ingredient> wrappingList = new List<Ingredient>();
     [SerializeField] List<Ingredient> cookingMethodList = new List<Ingredient>();
+    [SerializeField] private List<Dumpling> dishMadeList = new List<Dumpling>();
 
     public void FillingUpdate()
     {
        Ingredient currentFilling = fillingList[Random.Range(0, fillingList.Count)];
-       ingredientName.text = currentFilling.ingredientName;
-       ingredientPrefab.GetComponent<SpriteRenderer>().sprite = currentFilling.ingredientSprite;
-    }
-
-    public void CookingMethod()
-    {
-        Ingredient currentCooking = cookingMethodList[Random.Range(0, cookingMethodList.Count)];
-        ingredientName.text = currentCooking.ingredientName;
-        ingredientPrefab.GetComponent<SpriteRenderer>().sprite = currentCooking.ingredientSprite;
+       fillingName.text = currentFilling.ingredientName;
+       fillingPrefab.GetComponent<SpriteRenderer>().sprite = currentFilling.ingredientSprite;
     }
 
     public void WrappingMethod()
     {
         Ingredient currentWrapping = wrappingList[Random.Range(0, wrappingList.Count)];
-        ingredientName.text = currentWrapping.ingredientName;
-        ingredientPrefab.GetComponent<SpriteRenderer>().sprite = currentWrapping.ingredientSprite;
+        wrappingName.text = currentWrapping.ingredientName;
+        wrappingPrefab.GetComponent<SpriteRenderer>().sprite = currentWrapping.ingredientSprite;
     }
-    
-    
+    public void CookingMethod()
+    {
+        Ingredient currentCooking = cookingMethodList[Random.Range(0, cookingMethodList.Count)];
+        cookingMethodName.text = currentCooking.ingredientName;
+        cookingMethodPrefab.GetComponent<SpriteRenderer>().sprite = currentCooking.ingredientSprite;
+    }
+
+  
 }
