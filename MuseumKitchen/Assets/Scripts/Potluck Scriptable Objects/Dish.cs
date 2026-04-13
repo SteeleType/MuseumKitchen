4a9e8b3dcc59d26234e920e7732f24b6f0e31f7f
cxmi <christinexmi@gmail.com>
2026-04-13 12:33:33 -0400

    updated SOs

--- MuseumKitchen/Assets/Scripts/Potluck Scriptable Objects/Dish.cs
+++ MuseumKitchen/Assets/Scripts/Potluck Scriptable Objects/Dish.cs
@@ -4,6 +4,7 @@ using UnityEngine.UIElements;
 [CreateAssetMenu(fileName = "Dish", menuName = "Scriptable Objects/Dish")]
 public class Dish : ScriptableObject
 {
+    
     [SerializeField] private string dishName;
     [SerializeField] private Sprite dishSprite;
     [SerializeField] private string countryOfOrigin;
@@ -11,6 +12,15 @@ public class Dish : ScriptableObject
     [SerializeField] private Spice spice;
     [SerializeField] private CookingMethod cookingMethod;
     [SerializeField] private int distanceTraveled;
+    [SerializeField] private SpiceOrigin spiceOrigin;
+
+    void OnEnable()
+    {
+        spiceOrigin = SpiceManager.AddSpiceOrigin(spice);
+    }
+
 }
+
+
 
 
---
+++ MuseumKitchen/Assets/Scripts/Potluck Scriptable Objects/SpiceOrigin.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public enum SpiceOrigin
+{
+    India,
+    SriLanka,
+    Iran,
+    Indonesia,
+    Mediterranean,
+}
